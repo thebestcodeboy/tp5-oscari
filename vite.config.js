5b571697e5ac5c2bd1f9ac8ae5038fc789b92d18
thebestcodeboy <oscarilucio4@gmail.com>
2025-10-07 23:06:06 -0300

    TP5 inicial

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tp4-oscari/',   // exacto al nombre dell repo
+  base: '/tp5-oscari/', 
 })
